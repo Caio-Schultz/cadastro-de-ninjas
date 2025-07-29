dd3b612de81b68486ffa233217bf638a29f27cf2
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-07-29 16:58:29 -0300

    refactor: refatora o Controller de missões para incluir ResponseEntity nas rotas e customizar as respostas das requisições

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/missoes/MissoesController.java
@@ -1,5 +1,7 @@
 package dev.java10x.CadastroDeNinjas.missoes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,32 +19,68 @@ public class MissoesController {
 
     // adicionar missão (CREATE)
     @PostMapping("/criar")
-    public MissoesDTO criarMissao(@RequestBody MissoesDTO missao){
-        return missoesService.criarMissao(missao);
+    public ResponseEntity<String> criarMissao(@RequestBody MissoesDTO missao){
+        MissoesDTO missaoCriada = missoesService.criarMissao(missao);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Missão criada com sucesso: " + missaoCriada.getNomeMissao() + " (ID: " + missaoCriada.getId() + ")");
     }
 
     // mostrar todas as missões (READ)
     @GetMapping("/listar")
-    public List<MissoesDTO> listarMissoes(){
-        return missoesService.listarMissoes();
+    public ResponseEntity<?> listarMissoes(){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        if(!missoes.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(missoes);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não há nenhuma missão na lista");
+        }
     }
 
     // mostrar missão por id (READ)
     @GetMapping("/listar/{id}")
-    public MissoesDTO listarMissaoPorId(@PathVariable Long id){
-        return missoesService.listarMissaoPorId(id);
+    public ResponseEntity<?> listarMissaoPorId(@PathVariable Long id){
+        if(missoesService.listarMissaoPorId(id) != null){
+            MissoesDTO missaoListada = missoesService.listarMissaoPorId(id);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(missaoListada);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("ID " + id + " não corresponde a nenhuma missão!");
+        }
     }
 
     // alterar dados da missão por id (UPDATE)
     @PutMapping("/atualizar/{id}")
-    public MissoesDTO atualizarMissaoPorId(@PathVariable Long id, @RequestBody MissoesDTO missao){
-        return missoesService.atualizarMissaoPorId(id, missao);
+    public ResponseEntity<String> atualizarMissaoPorId(@PathVariable Long id, @RequestBody MissoesDTO missao){
+        if(missoesService.listarMissaoPorId(id) != null){
+            MissoesDTO missaoAtualizada = missoesService.atualizarMissaoPorId(id, missao);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Missão \"" + missaoAtualizada.getNomeMissao() + "\" atualizada com sucesso!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("ID " + id + " não corresponde a nenhuma missão!");
+        }
+
     }
 
     // deletar missão por id (DELETE)
     @DeleteMapping("/deletar/{id}")
-    public void deletarMissaoPorId(@PathVariable Long id) {
-        missoesService.deletarMissaoPorId(id);
+    public ResponseEntity<String> deletarMissaoPorId(@PathVariable Long id) {
+        if(missoesService.listarMissaoPorId(id) != null){
+            missoesService.deletarMissaoPorId(id);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Missão com ID " + id + " deletada com sucesso!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("ID " + id + " não corresponde a nenhuma missão!");
+        }
+
     }
 
 }
